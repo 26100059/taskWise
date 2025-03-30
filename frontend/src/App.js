0acff2e6aa3c2a0e1144fc32cef6a06356ce5d72
26100059 <26100059@lums.edu.pk>
2025-03-30 15:42:27 +0500

    fixed redux, landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess } from "./redux/authSlice";
 
 import MainPage from "./pages/mainPage";
 import SignUpPage from "./pages/signUp";
@@ -9,36 +10,39 @@ import DashboardPage from "./pages/dashboard";
 import ProfilePage from "./pages/profilePage";
 import TestingDB from "./pages/TestingDB";
 
-
 console.log("🔹 App.js is being loaded!");
 
-// Protected Route Component
+// Protected Route Component for routes that require authentication
 const ProtectedRoute = ({ children }) => {
   const user = useSelector((state) => state.auth.user);
-  if (user === null) {
+  if (!user) {
     console.log("🔴 No user found, redirecting to Sign In...");
     return <Navigate to="/signin" replace />;
   }
-
   return children;
 };
 
-
 function App() {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     console.log("📂 LocalStorage User:", storedUser ? JSON.parse(storedUser) : "No user found");
-  }, []);
+    if (storedUser) {
+      // Initialize Redux state from localStorage if user data exists
+      dispatch(loginSuccess(JSON.parse(storedUser)));
+    }
+  }, [dispatch]);
 
   return (
     <Router>
       <Routes>
         {/* Public Routes */}
+        <Route path="/" element={<MainPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
 
         {/* Protected Routes */}
-        <Route path="/" element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
         <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
         <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
         <Route path="/testingDB" element={<ProtectedRoute><TestingDB /></ProtectedRoute>} />
